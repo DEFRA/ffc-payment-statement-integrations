6e26064bd6b940536c6251d10b167b6326d4510e
work <jeremy.barnsley@esynergy.co.uk>
2023-06-02 16:14:51 +0100

    Re-aligned variable names

--- PaymentStatementIntegrations.Tests/CrmInsertTest/CrmInsertTest.cs
+++ PaymentStatementIntegrations.Tests/CrmInsertTest/CrmInsertTest.cs
@@ -73,6 +73,52 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
             }
         }
 
+        /// <summary>
+        /// Tests that the correct response is returned when successful.
+        /// </summary>
+        [TestMethod]
+        public void CrmInsertTest_Fails_When_Invalid_JSON()
+        {
+            // Override one of the settings in the local settings file
+            var settingsToOverride = new Dictionary<string, string>();
+
+            using (ITestRunner testRunner = CreateTestRunner(settingsToOverride))
+            {
+                // Mock the HTTP calls and customize responses
+                testRunner.AddApiMocks = (request) =>
+                {
+                    HttpResponseMessage mockedResponse = new HttpResponseMessage();
+                    if (request?.RequestUri != null && request.RequestUri.AbsolutePath.Contains("/api/data/v9.1/") && request.Method == HttpMethod.Post)
+                    {
+                        mockedResponse.RequestMessage = request;
+                        mockedResponse.StatusCode = HttpStatusCode.OK;
+                        mockedResponse.Content = ContentHelper.CreatePlainStringContent("success");
+                    }
+                    return mockedResponse;
+                };
+
+                // Run the workflow
+                var workflowResponse = testRunner.TriggerWorkflow(
+                    GetServiceBusMessageInvalidJson(),
+                    HttpMethod.Post);
+
+                // Check workflow run status
+                Assert.AreEqual(WorkflowRunStatus.Succeeded, testRunner.WorkflowRunStatus);
+
+                // Check workflow response
+                testRunner.ExceptionWrapper(() => Assert.AreEqual(HttpStatusCode.OK, workflowResponse.StatusCode));
+                Assert.AreEqual(HttpStatusCode.OK, workflowResponse.StatusCode);
+
+                // Check action result
+                Assert.AreEqual(ActionStatus.Succeeded, testRunner.GetWorkflowActionStatus("Add_a_row_to_CRM"));
+
+                // Check request to CRM via Dataserve connector never happened
+                var crmRequest = testRunner.MockRequests.First(r => r.RequestUri.AbsolutePath.Contains("/api/data/v9.1"));
+                Assert.AreEqual(HttpMethod.Post, crmRequest.Method);
+                Assert.IsFalse(crmRequest.Content.Contains("FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf"));
+            }
+        }
+
         private static StringContent GetServiceBusMessage()
         {
             return ContentHelper.CreateJsonStringContent(new
@@ -85,6 +131,34 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
                     apiLink = "https://myStatementRetrievalApiEndpoint/statement-receiver/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf",
                     documentType = "Payment statement",
                     scheme = "SFI",
+                },
+                contentType = "application/json",
+                messageId = "ff421d65-5be6-4084-b748-af490100c9a5",
+                label = "customer.54624",
+                scheduledEnqueueTimeUtc = "1/1/0001 12:00:00 AM",
+                sessionId = "54624",
+                timeToLive = "06:00:00",
+                deliveryCount = 1,
+                enqueuedSequenceNumber = 6825,
+                enqueuedTimeUtc = "2022-11-10T15:34:57.727Z",
+                lockedUntilUtc = "9999-12-31T23:59:59.9999999Z",
+                lockToken = "056bb9fa-9b8f-4d93-874b-7e78e71a588d",
+                sequenceNumber = 980
+            });
+        }
+
+        private static StringContent GetServiceBusMessageInvalidJson()
+        {
+            return ContentHelper.CreateJsonStringContent(new
+            {
+                // The JSON must match the data structure used by the Service Bus trigger, this includes 'contentData' to represent the message content
+                contentData = new
+                {
+                    sbi2 = 12345678,
+                    frn2 = 123456789,
+                    apiLink2 = "https://myStatementRetrievalApiEndpoint/statement-receiver/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf",
+                    documentType2 = "Payment statement",
+                    scheme2 = "SFI",
                 },
                 contentType = "application/json",
                 messageId = "ff421d65-5be6-4084-b748-af490100c9a5",
