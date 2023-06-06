7bb56b3cda6cb5299090b3f0e8dfbd873d0333bb
work <jeremy.barnsley@esynergy.co.uk>
2023-06-06 15:30:36 +0100

    setup vars

---
+++ cd/setup_vars_common.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+IFS=$'\n\t'
+
+# Creating a build manifest to pass variables to later steps in the pipeline
+PROJECT_NAME="logicappstda"
+ARTIFACT_NAME="deploy_artifacts"
+# API Connections - if you change this name, you must change the reference in workflow.json
+BLOB_CONNECTION_NAME="azureblob"
+
+# Copy to environment vars
+echo "PROJECT_NAME=$PROJECT_NAME" >> $GITHUB_ENV
+echo "ARTIFACT_NAME=$ARTIFACT_NAME" >> $GITHUB_ENV
+echo "BLOB_CONNECTION_NAME=$BLOB_CONNECTION_NAME" >> $GITHUB_ENV
+EOF
