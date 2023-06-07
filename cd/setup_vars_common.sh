67f26daa9336b827bed3ed78084af06bcbdc1be9
work <jeremy.barnsley@esynergy.co.uk>
2023-06-07 09:22:31 +0100

    artifact path

--- cd/setup_vars_common.sh
+++ cd/setup_vars_common.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 # Creating a build manifest to pass variables to later steps in the pipeline
 PROJECT_NAME="logicappstda"
-ARTIFACT_NAME="deploy_artifacts"
+ARTIFACT_NAME="build-artifact"
 # API Connections - if you change this name, you must change the reference in workflow.json
 BLOB_CONNECTION_NAME="azureblob"
 
