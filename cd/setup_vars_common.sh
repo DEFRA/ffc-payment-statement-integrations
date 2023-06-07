08a83438a62281e8c89d2102ade13f196d83e140
work <jeremy.barnsley@esynergy.co.uk>
2023-06-07 10:14:10 +0100

    changed proj name

--- cd/setup_vars_common.sh
+++ cd/setup_vars_common.sh
@@ -3,7 +3,7 @@ set -eu
 IFS=$'\n\t'
 
 # Creating a build manifest to pass variables to later steps in the pipeline
-PROJECT_NAME="logicappstda"
+PROJECT_NAME="logicappstd"
 ARTIFACT_NAME="build-artifact"
 # API Connections - if you change this name, you must change the reference in workflow.json
 BLOB_CONNECTION_NAME="azureblob"
