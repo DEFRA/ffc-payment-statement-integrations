534b260f2a05e8139fea35a77f74eebc7a2f6142
work <jeremy.barnsley@esynergy.co.uk>
2023-06-15 08:38:57 +0100

    Tighter tests

--- PaymentStatementIntegrations.Tests/CrmInsertTest/CrmInsertTest.cs
+++ PaymentStatementIntegrations.Tests/CrmInsertTest/CrmInsertTest.cs
@@ -83,10 +83,10 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
         }
 
         /// <summary>
-        /// Tests that the correct response is returned when successful.
+        /// Tests that the correct response is returned when invalid JSON.
         /// </summary>
         [TestMethod]
-        public void CrmInsertTest_Fails_When_Invalid_JSON()
+        public void CrmInsertTest_Fails_When_Invalid_Schema_JSON()
         {
             // Override one of the settings in the local settings file
             var settingsToOverride = new Dictionary<string, string>();
@@ -108,7 +108,61 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
 
                 // Run the workflow
                 var workflowResponse = testRunner.TriggerWorkflow(
-                    GetServiceBusMessageInvalidJson(),
+                    GetServiceBusMessageInvalidSchemaJson(),
+                    HttpMethod.Post);
+
+                // Check workflow run status
+                Assert.AreEqual(WorkflowRunStatus.Succeeded, testRunner.WorkflowRunStatus);
+
+                // Check workflow response
+                testRunner.ExceptionWrapper(() => Assert.AreEqual(HttpStatusCode.Accepted, workflowResponse.StatusCode));
+                Assert.AreEqual(HttpStatusCode.Accepted, workflowResponse.StatusCode);
+
+                // Check action result
+                Assert.AreEqual(ActionStatus.Failed, testRunner.GetWorkflowActionStatus("Parse_JSON"));
+                Assert.AreEqual(ActionStatus.Skipped, testRunner.GetWorkflowActionStatus("Add_a_row_to_CRM"));
+                Assert.AreEqual(ActionStatus.Skipped, testRunner.GetWorkflowActionStatus("Dead-letter_the_message_in_a_topic_subscription_failed_CRM"));
+                Assert.AreEqual(ActionStatus.Succeeded, testRunner.GetWorkflowActionStatus("Dead-letter_the_message_in_a_topic_subscription_invalid_JSON"));
+                Assert.AreEqual(ActionStatus.Skipped, testRunner.GetWorkflowActionStatus("Complete_the_message_in_a_topic_subscription"));
+
+                // Check request to CRM via Dataserve connector never happened
+                var crmRequest = testRunner.MockRequests.FirstOrDefault(r => r.RequestUri.AbsolutePath.Contains("/api/data/v9.1"));
+                Assert.IsNull(crmRequest);
+
+                // Check tracked properties
+                var trackedProps = testRunner.GetWorkflowActionTrackedProperties("Parse_JSON");
+                var expectedBody = "{\"sbi2\":12345678,\"frn2\":123456789,\"apiLink2\":\"https://myStatementRetrievalApiEndpoint/statement-receiver/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf\",\"documentType2\":\"Payment statement\",\"scheme2\":\"SFI\",\"sbi\":\"defgh\"}";
+                Assert.AreEqual(expectedBody, trackedProps["messageBody"]);
+            }
+        }
+
+        /// <summary>
+        /// Tests that the correct response is returned when invalid JSON.
+        /// </summary>
+        [TestMethod]
+        public void CrmInsertTest_Fails_When_Missing_Items_In_JSON()
+        {
+            // Override one of the settings in the local settings file
+            var settingsToOverride = new Dictionary<string, string>();
+
+            using (ITestRunner testRunner = CreateTestRunner(settingsToOverride))
+            {
+                // Mock the HTTP calls and customize responses
+                testRunner.AddApiMocks = (request) =>
+                {
+                    HttpResponseMessage mockedResponse = new HttpResponseMessage();
+                    if (request?.RequestUri != null && request.RequestUri.AbsolutePath.Contains("/api/data/v9.1/") && request.Method == HttpMethod.Post)
+                    {
+                        mockedResponse.RequestMessage = request;
+                        mockedResponse.StatusCode = HttpStatusCode.OK;
+                        mockedResponse.Content = ContentHelper.CreatePlainStringContent("success");
+                    }
+                    return mockedResponse;
+                };
+
+                // Run the workflow
+                var workflowResponse = testRunner.TriggerWorkflow(
+                    GetServiceBusMessageValidButMissingItemsJson(),
                     HttpMethod.Post);
 
                 // Check workflow run status
@@ -131,7 +185,7 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
 
                 // Check tracked properties
                 var trackedProps = testRunner.GetWorkflowActionTrackedProperties("Parse_JSON");
-                var expectedBody = "{\"sbi2\":12345678,\"frn2\":123456789,\"apiLink2\":\"https://myStatementRetrievalApiEndpoint/statement-receiver/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf\",\"documentType2\":\"Payment statement\",\"scheme2\":\"SFI\"}";
+                var expectedBody = "{\"sbi\":12345678,\"frn\":123456789}";
                 Assert.AreEqual(expectedBody, trackedProps["messageBody"]);
             }
         }
@@ -222,7 +276,7 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
             });
         }
 
-        private static StringContent GetServiceBusMessageInvalidJson()
+        private static StringContent GetServiceBusMessageInvalidSchemaJson()
         {
             return ContentHelper.CreateJsonStringContent(new
             {
@@ -234,6 +288,32 @@ namespace PaymentStatementIntegrations.Tests.CrmInsertTest
                     apiLink2 = "https://myStatementRetrievalApiEndpoint/statement-receiver/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022090615023001.pdf",
                     documentType2 = "Payment statement",
                     scheme2 = "SFI",
+                    sbi = "defgh"
+                },
+                contentType = "application/json",
+                messageId = "ff421d65-5be6-4084-b748-af490100c9a5",
+                label = "customer.54624",
+                scheduledEnqueueTimeUtc = "1/1/0001 12:00:00 AM",
+                sessionId = "54624",
+                timeToLive = "06:00:00",
+                deliveryCount = 1,
+                enqueuedSequenceNumber = 6825,
+                enqueuedTimeUtc = "2022-11-10T15:34:57.727Z",
+                lockedUntilUtc = "9999-12-31T23:59:59.9999999Z",
+                lockToken = "056bb9fa-9b8f-4d93-874b-7e78e71a588d",
+                sequenceNumber = 980
+            });
+        }
+
+        private static StringContent GetServiceBusMessageValidButMissingItemsJson()
+        {
+            return ContentHelper.CreateJsonStringContent(new
+            {
+                // The JSON must match the data structure used by the Service Bus trigger, this includes 'contentData' to represent the message content
+                contentData = new
+                {
+                    sbi = 12345678,
+                    frn = 123456789
                 },
                 contentType = "application/json",
                 messageId = "ff421d65-5be6-4084-b748-af490100c9a5",
