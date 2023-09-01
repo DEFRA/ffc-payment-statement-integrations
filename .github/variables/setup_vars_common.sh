31786870fe4e2db9db7b821e3be8f2e820ffb321
work <jeremy.barnsley@esynergy.co.uk>
2023-09-01 10:54:22 +0100

    changed artifact name

--- .github/variables/setup_vars_common.sh
+++ .github/variables/setup_vars_common.sh
@@ -5,7 +5,6 @@ IFS=$'\n\t'
 # Use uppercase, with no hyphens or spaces
 PROJECT_NAME_UPPER="RLE"
 FUNCTION_CODE="INF"
-LOGIC_APP_ARTIFACT_NAME="logicapp_artifacts"
 
 # CSV list of any additional trusted CA certificate roots thumbprints
 # Palo Alto CSC Root CA, and *.defra.azure.cloud CA
@@ -14,5 +13,4 @@ TRUSTED_ROOT_THUMBPRINTS="BDB406F8EB50F8A56FCC8B1F95624B84A8A5E607,A8985D3A65E5E
 # Copy to environment vars
 echo "PROJECT_NAME_UPPER=$PROJECT_NAME_UPPER" >> $GITHUB_ENV
 echo "FUNCTION_CODE=$FUNCTION_CODE" >> $GITHUB_ENV
-echo "LOGIC_APP_ARTIFACT_NAME=$LOGIC_APP_ARTIFACT_NAME" >> $GITHUB_ENV
 echo "TRUSTED_ROOT_THUMBPRINTS=$TRUSTED_ROOT_THUMBPRINTS" >> $GITHUB_ENV
